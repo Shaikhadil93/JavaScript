f70ec31cc01a0042561b4a2f566672f3bd24ed71
Shaikhadil93 <shaikh87786@gmail.com>
2024-01-12 13:06:25 +0530

    colunm of serial number added successfully

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function addData() {
+
     let name = document.getElementById("nameInput").value;
     let email = document.getElementById("emailInput").value;
     let number = document.getElementById("numberInput").value;
@@ -7,46 +8,51 @@ function addData() {
     let table = document.getElementById("tableShow");
     let newRow = table.insertRow(table.rows.length);
 
-    newRow.insertCell(0).innerHTML = name;
-    newRow.insertCell(1).innerHTML = email;
-    newRow.insertCell(2).innerHTML = number;
-    newRow.insertCell(3).innerHTML = address;
-    newRow.insertCell(4).innerHTML = `<button onclick= "editData(this)"> Edit </button>` +
+    newRow.insertCell(0).innerHTML = "";
+    newRow.insertCell(1).innerHTML = name;
+    newRow.insertCell(2).innerHTML = email;
+    newRow.insertCell(3).innerHTML = number;
+    newRow.insertCell(4).innerHTML = address;
+    newRow.insertCell(5).innerHTML = `<button onclick= "editData(this)")"> Edit </button>` +
         `<button onclick= "deleteData(this)"> Delete </button>`
 
     clearInput();
 }
 
-
-
 function editData(Button) {
     let row = Button.parentNode.parentNode;
 
-    let nameCell = row.cells[0];
-    let emailCell = row.cells[1];
-    let numberCell = row.cells[2];
-    let addressCell = row.cells[3];
+    let nameCell = row.cells[1];
+    let emailCell = row.cells[2];
+    let numberCell = row.cells[3];
+    let addressCell = row.cells[4];
 
-    let nameInput =
-        prompt("Enter the updated name:",
-            nameCell.innerHTML);
-    let emailInput =
-        prompt("Enter the updated email:",
-            emailCell.innerHTML);
-    let numberInput =
-        prompt("Enter the updated mobile details:",
-            numberCell.innerHTML
-        );
-    let addressInput =
-        prompt("Enter the updated address:",
-            addressCell.innerHTML
-        );
+    let nameInput = document.getElementById("nameInput");
+    let emailInput = document.getElementById("emailInput");
+    let numberInput = document.getElementById("numberInput");
+    let addressInput = document.getElementById("addressInput");
 
-    nameCell.innerHTML = nameInput;
-    emailCell.innerHTML = emailInput;
-    numberCell.innerHTML = numberInput;
-    addressCell.innerHTML = addressInput;
+    nameInput.value = nameCell.innerHTML;
+    emailInput.value = emailCell.innerHTML;
+    numberInput.value = numberCell.innerHTML;
+    addressInput.value = addressCell.innerHTML; 
+
+
+    // nameCell.value= nameInput1.innerHTML;
+    // emailCell.value= emailInput1.innerHTML;
+    // numberCell.value= numberInput1.innerHTML;
+    // addressCell.value= addressInput1.innerHTML;
 }
+
+let button = document.getElementById("button");    
+button.addEventListener("change", changeData);
+function changeData(){
+    alert("change")
+}
+
+
+
+
 
 function deleteData(Button) {
     let row = Button.parentNode.parentNode;
